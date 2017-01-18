e864c02ffed84780dd07603f6e845b5295868fdc
Spencer <spencer.p.816@gmail.com>
2017-01-18 10:25:46 -0800

    Complete L2 lab

--- L2-DataViz/Complete.R
+++ L2-DataViz/Complete.R
@@ -5,10 +5,17 @@ library(ggplot2)
 data <- iris
 
 #basic scatter plot
-g<-ggplot(data, aes(Sepal.Length, Sepal.Width, color=factor(Species)))+geom_point()
+g <- ggplot(data,
+            aes(Sepal.Length,
+                Sepal.Width,
+                color= factor(Species))) +
+    geom_point()
+#   geom_point(color = "blue")
 
 #lets get a title and axis labels in the mix
-g<-g+labs(x="Sepal Length", y="Sepal Width", title='Sepal Length vs Sepal Width')
+g <- g + labs(x = "Sepal length",
+              y = "Sepal width",
+              title = "Sepal length vs Sepal width")
 
 #lets say we want to then color the graph by species of iris
 #color is then specified in aes because it is a function of the dataset
@@ -16,31 +23,37 @@ g<-g+labs(x="Sepal Length", y="Sepal Width", title='Sepal Length vs Sepal Width'
 
 
 #legend title is ugly
-g<- g+theme(legend.title=element_blank())
+
+g <- g + theme(legend.title = element_blank())
 
 #and the legend dots are tiny
-g <- g+ guides(color = guide_legend(override.aes = list(size = 4)))
-
+g <- g + guides(color = guide_legend(override.aes = list(size = 4)))
 
 #much better
 
 #shall we change the background color?
-g <- g + theme(panel.background = element_rect(fill="grey75"))
-
+g <- g + theme(panel.background = element_rect(fill = "grey75"))
 
 #hm you know I changed my mind about all the species being on one graph, lets put it on three
-g <- g + facet_wrap(~Species, nrow=1)
+g <- g + facet_wrap(~Species, nrow = 1)
 
 #lets add a line to those plots!
 #g <- g + stat_smooth()
 
 #that isn't quite what we were looking for, lets do a best fit instead
-g <- g + stat_smooth(method = "lm", se=FALSE, col="black")
+g <- g + stat_smooth(method = "lm",
+                     se = FALSE,
+                     col = "black")
+
+# g <- g + geom_smooth()
+
 
 #you can get all sorts of funky themes in the mix
 #https://github.com/jrnold/ggthemes
 library(ggthemes)
-g<- g+theme_economist()+scale_colour_economist()
+
+# g <- g + theme_economist() + scale_colour_economist()
+
 #NEAT
 
 #plotting
