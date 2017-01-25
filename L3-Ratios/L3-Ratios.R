43ed6a09df72690ad652e3178d759a8494349e0c
Spencer <spencer.p.816@gmail.com>
2017-01-25 10:19:51 -0800

    Work on lab assignment

--- L3-Ratios/L3-Ratios.R
+++ L3-Ratios/L3-Ratios.R
@@ -1,7 +1,8 @@
 #Welcome to Lab 3, please answer the following questions
 
 #Install and require package MASS
-
+library(MASS)
+library(dplyr)
 
 #Using the built in dataset anorexia, aggregate the data to look at:
 #       1. the number of patients who received a treatment (choose one) who responded positively to treatment
@@ -10,27 +11,47 @@
 #       4. the number of patients who were in the control group who did not respond or responded negatively to treatment
 
 #Such that you end up with a 2 by 2 table
+data <- anorexia %>%
+    mutate(weight.change = Postwt - Prewt)
 
+num.t.p <- data %>%
+    filter(Treat == 'CBT',
+           weight.change > 0) %>%
+    nrow()
+
+num.t.n <- data %>%
+    filter(Treat == 'CBT',
+           weight.change <= 0) %>%
+    nrow()
+
+num.c.p <- data %>%
+    filter(Treat == 'Cont',
+           weight.change > 0) %>%
+    nrow()
+
+num.c.n <- data %>%
+    filter(Treat == 'Cont',
+           weight.change <= 0) %>%
+    nrow()
 
 #What kind of analysis appropriate to perform on a study like this?
-
-
+#   It is most appropriate to perform a case-control analysis,
+#   as we have access to both test and control groups.
 
 
 #Perform the appropriate choice on the dataset
-
-
-
+result <- (num.t.p / num.t.n) / (num.c.p / num.c.n)
 
 #What does your result tell you?
+#   This result tells us the number of successful treatments for
+#   every unsuccessful treatment.
 
 
-
-
-
-#Write a function that allows you to perform your analysis based on what kind of treatment you want 
+#Write a function that allows you to perform your analysis based on what kind of treatment you want
 #that returns the appropriate calculation
+analysis <- function(treatment) {
 
+}
 
 
 
